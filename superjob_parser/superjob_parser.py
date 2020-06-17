be4658a7b4e6314d81cc6bcf81e8d26ad768fc79
Artiom Khandamirov <t9max@yandex.ru>
2020-06-17 15:45:19 +0300

    Fix module commentary

--- superjob_parser/superjob_parser.py
+++ superjob_parser/superjob_parser.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Script for booking database management."""
+"""Script for parsing students.superjob.ru internship offers."""
 import csv
 import time
 from typing import Optional, List, Dict, Any, Iterator, IO
